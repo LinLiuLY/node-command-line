390abf4e932b3599462854a9f92f1d5689a0e9f0
LinLiuSummer <lin.liu@myob.com>
2018-05-17 11:14:06 +1000

    Accept input during runtime.

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-// var program = require('commander');
-//
-// program
-//   .version('0.0.1')
-//   .option('-l, --list [list]', 'list of customers in CSV file')
-//   .parse(process.argv);
-//
-// console.log(program.list);
+//node index --list input/employees.csv
 
-const program = require('commander');
+var program = require('commander');
+var inquirer = require('inquirer');
+
 const csv = require('csv');
 const fs = require('fs');
 
 program
   .version('0.0.1')
-  .option('-l, --list [list]', 'List of customers in CSV')
-  .parse(process.argv)
+  .option('-l, --list [list]', 'list of customers in CSV file')
+  .parse(process.argv);
 
+//Runtime User Inputs
+//node index --list input/employees.csv
+let questions = [
+  {
+    type : "input",
+    name : "sender.email",
+    message : "Sender's email address - "
+  },
+  {
+    type : "input",
+    name : "sender.name",
+    message : "Sender's name - "
+  },
+  {
+    type : "input",
+    name : "subject",
+    message : "Subject - "
+  }
+];
+
+let contactList = [];
 let parse = csv.parse;
 let stream = fs.createReadStream(program.list)
-  .pipe(parse({ delimiter : ',' }));
+  .pipe(parse({ delimiter : "," }));
 
 stream
-  .on('data', function (data) {
-    let firstname = data[0];
-    let lastname = data[1];
+  .on("error", function (err) {
+    return console.error(err.message);
+  })
+  .on("data", function (data) {
+    let name = data[0] + " " + data[1];
     let email = data[2];
-    console.log(firstname, lastname, email);
+    contactList.push({ name : name, email : email });
+  })
+  .on("end", function () {
+    inquirer.prompt(questions).then(function (answers) {
+      console.log(answers);
+    });
   });
